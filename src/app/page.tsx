caef2448ea2d6cbaa6627edda3f4b40d142181a4
simonpfish <simonpfish@gmail.com>
2023-08-05 16:09:51 -0700

    consolidate logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,75 +1,50 @@
 "use client";
 
-import ScoreRadar from "@/components/ScoreRadar";
-import { Input } from "@/components/ui/input";
 import { useRef, useState, useCallback } from "react";
-import { Button } from "@/components/ui/button";
 import clsx from "clsx";
-import { SimilarityScores } from "@/types";
 import { ShuffleIcon } from "@radix-ui/react-icons";
+import ScoreRadar from "@/components/ScoreRadar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { EMOTIONS } from "@/constants";
+import { SimilarityScores } from "@/types";
 
 export default function Home() {
   const [text, setText] = useState("");
   const [scores, setScores] = useState<SimilarityScores>();
   const [isLoading, setIsLoading] = useState(false);
-
   const abortControllerRef = useRef<AbortController | null>(null);
 
-  const onShuffle = useCallback(() => {
-    const getRandom = () => {
-      setIsLoading(true);
+  const fetchScores = useCallback(
+    (inputs?: string[]) => {
+      if (isLoading && abortControllerRef.current)
+        abortControllerRef.current.abort();
+      abortControllerRef.current = new AbortController();
 
+      setIsLoading(true);
       fetch("/api/scores", {
         method: "POST",
+        body: JSON.stringify({ inputs }),
+        signal: abortControllerRef.current.signal,
       })
         .then((res) => res.json())
         .then((res) => {
           setScores(res);
-          setText(Object.keys(res).join(", "));
+          if (!inputs) setText(Object.keys(res).join(", "));
         })
-        .finally(() => {
-          setIsLoading(false);
-        });
-    };
+        .catch((error) => {
+          if (error.name !== "AbortError") throw error;
+        })
+        .finally(() => setIsLoading(false));
+    },
+    [isLoading]
+  );
 
-    getRandom();
-  }, []);
-
-  const onSubmit = useCallback(() => {
-    if (isLoading && abortControllerRef.current) {
-      abortControllerRef.current.abort();
-    }
-
-    if (!text) {
-      setScores(undefined);
-      return;
-    }
-
-    abortControllerRef.current = new AbortController();
-
-    setIsLoading(true);
-    fetch("/api/scores", {
-      method: "POST",
-      body: JSON.stringify({
-        inputs: text
-          .split(",")
-          .map((s) => s.trim())
-          .filter(Boolean),
-      }),
-      signal: abortControllerRef.current.signal,
-    })
-      .then((res) => res.json())
-      .then((res) => setScores(res))
-      .catch((error) => {
-        if (error.name === "AbortError") {
-          console.log("Fetch request was aborted");
-        } else {
-          throw error;
-        }
-      })
-      .finally(() => setIsLoading(false));
-  }, [isLoading, text]);
+  const onShuffle = useCallback(() => fetchScores(), [fetchScores]);
+  const onSubmit = useCallback(
+    () => text && fetchScores([text]),
+    [fetchScores, text]
+  );
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24 justify-center">
@@ -81,11 +56,9 @@ export default function Home() {
           )}
         >
           <Input
-            onChange={(e) => {
-              setText(e.target.value);
-            }}
             className="w-64"
             value={text}
+            onChange={(e) => setText(e.target.value)}
           />
           <Button type="submit" onClick={onSubmit} disabled={isLoading}>
             Calculate
@@ -95,7 +68,6 @@ export default function Home() {
           </Button>
         </div>
       </form>
-
       <div className="flex flex-col items-center h-96 font-mono w-[500px]">
         <ScoreRadar scores={scores} labels={Object.keys(EMOTIONS)} />
       </div>
