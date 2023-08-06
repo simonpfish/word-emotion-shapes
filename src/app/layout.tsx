98dee5aaae69d557b771a5af4b50914395e01e85
simonpfish <simonpfish@gmail.com>
2023-08-05 19:07:40 -0700

    rename package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Word Visuals",
+  title: "Word Shapes",
   description: "Visualize word sentiments",
   icons: [
     {
