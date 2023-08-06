fa0b7f00ac4795d1070359e04cfb1b82fbf4e796
simonpfish <simonpfish@gmail.com>
2023-08-05 18:57:50 -0700

    👀

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   icons: [
     {
       rel: "icon",
-      url: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔤</text></svg>",
+      url: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👀</text></svg>",
     },
   ],
 };
